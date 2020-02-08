69dea122358b9c3b67a2b2ef97b1737acd8a6a04
Golf Sinteppadon <golf.sinteppadon@gmail.com>
2020-02-08 00:12:11 -0800

    use exportAsync to grab actual rendered node bytes

--- code.ts
+++ code.ts
@@ -1,44 +1,36 @@
-async function createGroupContaining1PxRectangles(paint) {
-  // Only operate on images (but you could do it
-  // for solid paints and gradients if you wanted)
-  if (paint.type === 'IMAGE') {
-    // Paints reference images by their hash.
-    const image = figma.getImageByHash(paint.imageHash)
+async function createGroupContaining1PxRectangles(node: SceneNode) {
+  // Get the bytes for this image. However, the "bytes" in this
+  // context refers to the bytes of file stored in PNG format. It
+  // needs to be decoded into RGBA so that we can easily operate
+  // on it.
+  const bytes = await node.exportAsync()
 
-    // Get the bytes for this image. However, the "bytes" in this
-    // context refers to the bytes of file stored in PNG format. It
-    // needs to be decoded into RGBA so that we can easily operate
-    // on it.
-    const bytes = await image.getBytesAsync()
+  // Decoding to RGBA requires browser APIs that are only available
+  // within an iframe. So we create an invisible iframe to act as
+  // a "worker" which will do the task of decoding and send us a
+  // message when it's done. This worker lives in `decoder.html`
+  figma.showUI(__html__, { visible: false })
 
-    // Decoding to RGBA requires browser APIs that are only available
-    // within an iframe. So we create an invisible iframe to act as
-    // a "worker" which will do the task of decoding and send us a
-    // message when it's done. This worker lives in `decoder.html`
-    figma.showUI(__html__, { visible: false })
+  // Send the raw bytes of the file to the worker
+  figma.ui.postMessage(bytes)
 
-    // Send the raw bytes of the file to the worker
-    figma.ui.postMessage(bytes)
+  // Wait for the worker's response
+  const newBytes: {colors: RGBA[], width: number} = await new Promise((resolve, reject) => {
+    figma.ui.onmessage = value => resolve(value as {colors: RGBA[], width: number})
+  })
 
-    // Wait for the worker's response
-    const newBytes: {colors: RGBA[], width: number} = await new Promise((resolve, reject) => {
-      figma.ui.onmessage = value => resolve(value as {colors: RGBA[], width: number})
-    })
+  const pixels: RectangleNode[] = []
+  newBytes.colors.forEach((color: RGBA, i: number) => {
+    const pixel = figma.createRectangle()
+    pixel.fills = [{ type: 'SOLID', color: {r: color.r, g: color.g, b: color.b}, opacity: color.a }]
+    pixel.x = i % newBytes.width
+    pixel.y = Math.floor(i / newBytes.width)
+    pixel.name = `${rgbToHex(color.r, color.g, color.b)}${displayOpacity(color.a)}`
+    pixel.resizeWithoutConstraints(1, 1)
+    pixels.push(pixel)
+  })
 
-    const pixels: RectangleNode[] = []
-    newBytes.colors.forEach((color: RGBA, i: number) => {
-      const pixel = figma.createRectangle()
-      pixel.fills = [{ type: 'SOLID', color: {r: color.r, g: color.g, b: color.b}, opacity: color.a }]
-      pixel.x = i % newBytes.width
-      pixel.y = Math.floor(i / newBytes.width)
-      pixel.name = `${rgbToHex(color.r, color.g, color.b)}${displayOpacity(color.a)}`
-      pixel.resizeWithoutConstraints(1, 1)
-      pixels.push(pixel)
-    })
-
-    return figma.group(pixels, figma.currentPage)
-  }
-  return null
+  return figma.group(pixels, figma.currentPage)
 }
 
 function displayOpacity(opacity: number) {
@@ -59,37 +51,16 @@ function rgbToHex(r: number, g: number, b: number) {
 async function main(): Promise<string | undefined> {
   // Make sure the selection is a single piece of text before proceeding.
   if (figma.currentPage.selection.length !== 1) {
-    return "Please select a single node with a fill"
+    return "Please select a single node when running this plugin"
   }
 
   const node = figma.currentPage.selection[0]
-  // Look for fills on node types that have fills.
-  // An alternative would be to do `if ('fills' in node) { ... }
-  switch (node.type) {
-    case 'RECTANGLE':
-    case 'ELLIPSE':
-    case 'POLYGON':
-    case 'STAR':
-    case 'VECTOR':
-    case 'TEXT': {
-      // Create a new array of fills, because we can't directly modify the old one
-      for (const paint of (node as any).fills) {
-        const group = await createGroupContaining1PxRectangles(paint)
-        if (group) {
-          group.x = node.x
-          group.y = node.y
-          group.name = `${node.name} 1px rectangles`
+  const group = await createGroupContaining1PxRectangles(node)
+  group.x = node.x
+  group.y = node.y
+  group.name = `${node.name} 1px rectangles`
 
-          node.remove()
-        }
-      }
-      break
-    }
-
-    default: {
-      // not supported, silently do nothing
-    }
-  }
+  node.remove()
 }
 
 main().then((message: string | undefined) => {
